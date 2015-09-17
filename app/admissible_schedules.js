7582094387f9a95becc8dadabda9f615801c0d2e
Danny <pacurici_daniel2004@yahoo.com>
2015-09-17 12:07:40 +0200

    add spec tests and functionality

--- app/admissible_schedules.js
+++ app/admissible_schedules.js
@@ -1,3 +1,37 @@
 /**
  * Created by daniel.pacurici on 17.09.2015.
  */
+module.exports = {
+    hasDependencies: hasDependencies,
+    canProcess: canProcess,
+    process: process
+};
+
+function hasDependencies(part) {
+    return (part.dependencies && part.dependencies.length > 0) > 0;
+}
+
+function canProcess(carParts, part) {
+    var processable = true;
+    if (!hasDependencies(carParts[part])) {
+        return processable;
+    }
+    else {
+        carParts[part].dependencies.forEach(function (dependency) {
+            processable = processable && !!carParts[dependency].state;
+        });
+    }
+    return processable;
+}
+
+function process(carParts, part) {
+    carParts[part].dependencies.forEach(function (dependency) {
+        if (canProcess(carParts, dependency)) {
+            carParts[dependency].state = 1;
+        }
+        else {
+            process(carParts, dependency);
+            carParts[dependency].state = 1;
+        }
+    })
+}
--- spec/admissibleScheduleSpec.js
+++ spec/admissibleScheduleSpec.js
@@ -1,10 +1,77 @@
 /**
  * Created by daniel.pacurici on 17.09.2015.
  */
-describe('check the setup', function(){
-    describe('2*3', function(){
-        it("should return 6", function(){
-            expect(2*3).toBe(6);
+
+var parts = require('../app/admissible_schedules.js');
+
+describe('admissible schedule setup', function () {
+    var carParts;
+    beforeEach(function () {
+        carParts = {
+            1: {state: 0, dependencies: null}, 2: {state: 0, dependencies: [1, 4]},
+            3: {state: 0, dependencies: [2]}, 4: {state: 0, dependencies: [5]},
+            5: {state: 0, dependencies: [6]}, 6: {state: 0, dependencies: null}
+        };
+    });
+
+    describe('when product has dependencies', function () {
+        var part, hasDependencies;
+        beforeEach(function () {
+            part = carParts[2];
+            hasDependencies = parts.hasDependencies(part);
+        });
+
+        it("should return true", function () {
+            expect(hasDependencies).toBe(true);
+        });
+
+        describe("and dependencies are in state 0", function () {
+            var part,
+                canProcessBeforeProcessing,
+                canProcessAfterProcessing;
+
+            beforeEach(function () {
+                part = 2;
+                canProcessBeforeProcessing = parts.canProcess(carParts, part);
+                parts.process(carParts, part);
+                canProcessAfterProcessing = parts.canProcess(carParts, part);
+            });
+
+            it("should return false", function () {
+                expect(canProcessBeforeProcessing).toBe(false);
+            });
+
+            it("should 'manufacture' the dependencies parts", function () {
+                expect(canProcessAfterProcessing).toBe(true)
+            })
+
+        });
+
+        describe("and dependencies are in state 1", function () {
+            var canProcess, part;
+            beforeEach(function () {
+                carParts[1].state = 1;
+                carParts[4].state = 1;
+                part = 2;
+                canProcess = parts.canProcess(carParts, part);
+            });
+
+            it("should return true", function () {
+                expect(canProcess).toBe(true);
+            });
+        });
+    });
+
+    describe("product doesn't have dependencies", function () {
+        var part, hasDependencies;
+        beforeEach(function () {
+            part = carParts[1];
+            hasDependencies = parts.hasDependencies(part);
+        });
+
+        it("should return false", function () {
+            expect(hasDependencies).toBe(false);
         })
     });
 });
+
